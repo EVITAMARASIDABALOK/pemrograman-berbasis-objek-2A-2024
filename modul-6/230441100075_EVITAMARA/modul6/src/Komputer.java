d0d2dd1c45be1fcebe34114bd6164008a1820856
EVITAMARASIDABALOK <evitamara825@gmail.com>
2024-06-03 16:47:22 +0700

    230441100075_EVITAMARA_asprakkarizqy

---
+++ modul-6/230441100075_EVITAMARA/modul6/src/Komputer.java
@@ -0,0 +1,8 @@
+abstract class Komputer implements Mouse,Keyboard,Printer{
+    abstract void hidupkan_os();
+    abstract void matikan_os();
+
+    public void cetak_data() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
---
+++ modul-6/230441100075_EVITAMARA/modul6/src/Notebook.java
@@ -0,0 +1,47 @@
+public class Notebook extends Komputer implements Mouse,Keyboard,Printer{
+    void hidupkan_os() {
+        System.out.println("----------------");
+        System.out.println("     NOTEBOOK    ");
+        System.out.println("----------------");
+        System.out.println("Hidupkan os");
+    }
+
+    void matikan_os() {
+        System.out.println("----------------");
+        System.out.println("     NOTEBOOK    ");
+        System.out.println("----------------");
+        System.out.println("Matikan os");
+    }
+
+    @Override
+    public void klik_kanan() {
+        System.out.println("----------------");
+        System.out.println("     NOTEBOOK    ");
+        System.out.println("----------------");
+        System.out.println("Klik kanan");
+    }
+
+    @Override
+    public void klik_kiri() {
+        System.out.println("----------------");
+        System.out.println("     NOTEBOOK    ");
+        System.out.println("----------------");
+        System.out.println("Klik kiri");
+    }
+
+    @Override
+    public void tekan_enter() {
+        System.out.println("----------------");
+        System.out.println("     NOTEBOOK    ");
+        System.out.println("----------------");
+        System.out.println("Tekan enter");
+    }
+
+    @Override
+    public void cetak_data() {
+        System.out.println("----------------");
+        System.out.println("     NOTEBOOK    ");
+        System.out.println("----------------");
+        System.out.println("Cetak data");
+    }
+}
